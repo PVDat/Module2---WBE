4dd88691e2c1a47ea58438e90c4ff9fc9ccb410e
PVDat <dat6a6zz@gmail.com>
2019-08-30 09:20:57 +0700

    Session 1 thực hành hiển thị thời gian hệ thống

---
+++ [SS1-TH]Systemtime/src/Blebleble/Main.java
@@ -0,0 +1,11 @@
+package Blebleble;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Date is: " + now);
+    }
+}
